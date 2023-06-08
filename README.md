eb410a4369557e8090ccba9e30ea6defd94c5672
codewithmitesh <codewithmitesh@gmail.com>
2023-06-08 17:01:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TicTacToe Dual Player Game
+
+# new Flutter Game :- TicTacToe Game developed by Mitesh 
---
+++ lib/elements/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  // #EA6262
+  static Color primaryColor = Color.fromRGBO(102, 70, 222, 1.00);
+  static Color secondaryColor = Color.fromARGB(255, 129, 208, 12);
+  static Color accentColor = Color.fromARGB(255, 192, 126, 41);
+}
